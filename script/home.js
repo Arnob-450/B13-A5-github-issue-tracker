542b456192b7b723674317e0233c49648fa92180
Arnob-450 <tawhidkhanarnob@gmail.com>
2026-03-16 16:02:43 +0600

    spinner is added  and the project is finished

--- script/home.js
+++ script/home.js
@@ -1,5 +1,15 @@
 console.log("Home page initialized.");
 
+const manageSpinner = (status) => {
+  if (status==true) {
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("card-container").classList.add("hidden");
+  } else {
+    document.getElementById("card-container").classList.remove("hidden");
+    document.getElementById("spinner").classList.add("hidden");
+}
+};
+
 function setActiveButton(activeId) {
     document.getElementById('all-btn').className = 'btn btn-outline btn-ghost btn-primary';
     document.getElementById('open-btn').className = 'btn btn-outline btn-ghost btn-primary';
@@ -10,6 +20,7 @@ function setActiveButton(activeId) {
 let currentIssues = [];
 
 function loadAll(){
+     manageSpinner(true);
     fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
     .then(res=>res.json())
     .then(data=>{
@@ -20,6 +31,7 @@ function loadAll(){
 }
 
 function loadOpen(){
+    manageSpinner(true);
     fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
     .then(res=>res.json())
     .then(data=>{
@@ -31,6 +43,7 @@ function loadOpen(){
 }
 
 function loadClosed(){
+    manageSpinner(true);
     fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
     .then(res=>res.json())
     .then(data=>{
@@ -156,6 +169,7 @@ const displayAll=(data)=>{
         `
         cardContainer.appendChild(card);
     });
+    manageSpinner(false);
 }
 
 document.getElementById('input-search').addEventListener('input', () => {
